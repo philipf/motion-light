23e193fcdb6cab9b64225d253af57913d7a641e7
Philip Fourie <philip.fourie@gmail.com>
2015-02-07 14:18:37 +0200

    Improvements added to switch off logic. Added ability to override the relay and always keep it open.

--- motion_light/motion_light.ino
+++ motion_light/motion_light.ino
@@ -15,45 +15,60 @@ by Philip Fourie
 
 
 // Pin allocations
-const int pirPin      = 7;
-const int relayPin    = 13;
-const int overridePin = 2;
+const int pirPin   = 7;
+const int relayPin = 2;
+const int relayLed = 13;
+const int overridePin = 4;
 
-int lightOnTime = 30000; // light has to stay on for 30 seconds
+unsigned int lightOnTime = 10000; // Minimum time for which light has to stay on
+unsigned long previousHigh = 0;
 
 void setup() {
   pinMode(pirPin, INPUT);
-  pinMode(overridePin, INPUT);
   pinMode(relayPin, OUTPUT);
+  
+  pinMode(overridePin, INPUT);
 }
 
 void loop() {
-  if (isOverrided()) {
+  if (isOverride()) {
+    relayOn();
+    digitalWrite(relayLed, HIGH);
     delay(300);
     return;
   }
   
-  if (isMovementDetected()) {
+  digitalWrite(relayLed, LOW);
+  
+  unsigned long currentMillis = millis();
+  
+  int pirState = digitalRead(pirPin);
+  
+  // Automatically extend the period for which the light should stay on.
+  // This is important to prevent the problem of switching the lights off 
+  // when movement was detected a just couple of seconds ago.
+  // In other words the window for the light stays on is extended everytime movement
+  // is detected.
+  if (pirState == HIGH) {
     relayOn();
-    delay(lightOnTime);
-  } else {
+    previousHigh = currentMillis;
+  } else if ((unsigned long)(currentMillis - previousHigh) >= lightOnTime) {
     relayOff();
-    delay(300);
   }
-}
-
-bool isOverrided() {
-    return digitalRead(overridePin) == 1;
-}
-
-bool isMovementDetected() {
-  return digitalRead(pirPin) == 1;
+   
+  delay(300);
 }
 
 void relayOn() {
-  digitalWrite(relayPin, HIGH);  
+  digitalWrite(relayPin, HIGH);
+ // digitalWrite(relayLed, HIGH);  
 }
 
 void relayOff() {
   digitalWrite(relayPin, LOW);
+//  digitalWrite(relayLed, LOW);    
+}
+
+bool isOverride() {
+  return (digitalRead(overridePin) == HIGH);
 }
